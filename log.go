b645c8095e73b1df2f04e48b12dff9edcfd150ce
Simon Hessel <simon.hessel@kreios.lu>
2020-09-17 14:23:14 +0200

    updated col.or package

--- log.go
+++ log.go
@@ -3,15 +3,15 @@ package log
 import (
 	"fmt"
 
-	"github.com/fatih/color"
+	. "github.com/logrusorgru/aurora"
 )
 
-func prefix(prefix string, c color.Attribute) func(format string, a ...interface{}) {
+func prefix(prefix string, color func(interface{}) Value) func(format string, a ...interface{}) {
 	return func(format string, a ...interface{}) {
-		color.New(c).Printf(fmt.Sprintf("[%s] %s\n", prefix, format), a...)
+		fmt.Println(color(fmt.Sprintf(fmt.Sprintf("[%s] %s", prefix, format), a...)))
 	}
 }
 
-var Info = prefix("Info", color.FgGreen)
-var Warning = prefix("Warning", color.FgYellow)
-var Error = prefix("Error", color.FgRed)
+var Info = prefix("Info", Green)
+var Warning = prefix("Warning", Yellow)
+var Error = prefix("Error", Red)
